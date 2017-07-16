6179a51ac3569c54248210faf60c8456d2571914
whywherewhat <mdsektor@gmail.com>
2017-07-16 20:22:51 +0100

    Added 05.FlattenDictionary Added 06.CottageScraper

---
+++ 10.LambdaAndLINQ-Exercises/05.FlattenDictionary/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.FlattenDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> infoDict = new Dictionary<string, Dictionary<string, string>>();
+
+            Dictionary<string, List<string>> flattened = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] tokens = input.Split(' ');
+                string mainKey = tokens[0];
+                string innerKey = tokens[1];
+
+                if (tokens.Length == 3)
+                {
+                    string innerValue = tokens[2];
+                    if (!infoDict.ContainsKey(mainKey))
+                    {
+                        infoDict.Add(mainKey, new Dictionary<string, string>());
+                    }
+
+                    if (!infoDict[mainKey].ContainsKey(innerKey))
+                    {
+                        infoDict[mainKey].Add(innerKey, innerValue);
+                    }
+                    else
+                    {
+                        infoDict[mainKey][innerKey] = innerValue;
+                    }
+                }
+                else
+                {
+                    foreach (var pair in infoDict[innerKey])
+                    {
+                        string text = pair.Key + pair.Value;
+                        if (!flattened.ContainsKey(innerKey))
+                        {
+                            flattened.Add(innerKey, new List<string>());
+                        }
+                        flattened[innerKey].Add(text);
+                    }
+                    infoDict[innerKey] = new Dictionary<string, string>();
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var pair in infoDict.OrderByDescending(x => x.Key.Length))
+            {
+                int count = 1;
+                Console.WriteLine($"{pair.Key}");
+                var key = pair.Key;
+                foreach (var secondPair in pair.Value.OrderBy(x => x.Key.Length))
+                {
+                    if (secondPair.Value != "null")
+                    {
+                        Console.WriteLine($"{count}. {secondPair.Key} - {secondPair.Value}");
+                        count++;
+                    }
+                }
+                if (flattened.ContainsKey(key))
+                {
+                    foreach (var flatPair in flattened[key])
+                    {
+                        Console.WriteLine($"{count}. {flatPair}");
+                        count++;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ 10.LambdaAndLINQ-Exercises/06.CottageScraper/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.CottageScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<int>> data = new Dictionary<string, List<int>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "chop chop")
+            {
+                string[] tokens = input.Split(new string[] {" -> "}, StringSplitOptions.RemoveEmptyEntries);
+
+                string treeType = tokens[0];
+                int treeHeight = int.Parse(tokens[1]);
+
+                if (! data.ContainsKey(treeType))
+                {
+                    data.Add(treeType, new List<int>());
+                }
+                data[treeType].Add(treeHeight);
+
+                input = Console.ReadLine();
+            }
+
+            string typeNeeded = Console.ReadLine();
+            int heightNeeded = int.Parse(Console.ReadLine());
+
+            double sumTrees = data.Values.Sum(d => d.Sum());
+            int treesCount = 0;
+            foreach (var list in data.Values)
+            {
+                treesCount += list.Count;
+            }
+            double pricePerMeter = Math.Round(sumTrees / treesCount, 2);
+
+            Console.WriteLine($"Price per meter: ${pricePerMeter:f2}");
+
+            int usedLogs = 0;
+
+            //double temp = data.Values.SelectMany(element => element).ToList().Average();
+
+            foreach (KeyValuePair<string, List<int>> pair in data)
+            {
+                if (pair.Key == typeNeeded)
+                {
+                    foreach (int treeH in data[pair.Key])
+                    {
+                        if (treeH >= heightNeeded)
+                        {
+                            usedLogs += treeH;
+                        }
+                    }
+                }
+            }
+
+            int unusedLogs = (int)sumTrees - usedLogs;
+
+            double usedLogsPrice = Math.Round(usedLogs * pricePerMeter, 2);
+            double unusedLogsPrice = Math.Round(unusedLogs * pricePerMeter * 0.25, 2);
+            double subTotal = Math.Round(usedLogsPrice + unusedLogsPrice, 2);
+
+            Console.WriteLine($"Used logs price: ${usedLogsPrice:f2}");
+            Console.WriteLine($"Unused logs price: ${unusedLogsPrice:f2}");
+            Console.WriteLine($"CottageScraper subtotal: ${subTotal:f2}");
+        }
+    }
+}
