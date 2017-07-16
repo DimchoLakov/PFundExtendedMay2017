f835742e91d9532d985629634a21fba067a824a5
whywherewhat <mdsektor@gmail.com>
2017-07-16 16:58:07 +0100

    Added 04.SoftuniBeerPong

---
+++ 10.LambdaAndLINQ-Exercises/04.SoftUniBeerPong/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.SoftUniBeerPong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, Dictionary<string, double>> teams = new Dictionary<string, Dictionary<string, double>>();
+
+            while (input != "stop the game")
+            {
+                string[] tokens = input.Split('|');
+                string playerName = tokens[0];
+                string teamName = tokens[1];
+                double points = double.Parse(tokens[2]);
+
+                if (!teams.ContainsKey(teamName))
+                {
+                    teams.Add(teamName, new Dictionary<string, double>());
+                }
+
+                if (teams[teamName].Count < 3)
+                {
+                    if (!teams[teamName].ContainsKey(playerName))
+                    {
+                        teams[teamName].Add(playerName, points);
+                    }
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            int count = 1;
+
+            foreach (KeyValuePair<string, Dictionary<string, double>> team in teams.OrderByDescending(x => x.Value.Values.Sum()))
+            {
+                if (team.Value.Count == 3)
+                {
+                    Console.WriteLine($"{count}. {team.Key}; Players:");
+                    foreach (KeyValuePair<string, double> player in team.Value.OrderByDescending(x => x.Value))
+                    {
+                        Console.WriteLine($"###{player.Key}: {player.Value}");
+                    }
+                    count++;
+                }
+                
+            }
+        }
+    }
+}
