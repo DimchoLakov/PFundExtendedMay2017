2ca1478d887ca08e2a16edd5822bbfc90ef2383f
whywherewhat <mdsektor@gmail.com>
2017-07-21 12:43:08 +0100

    Added 04.DistanceBetweenPoints

---
+++ 11.ObjectsAndSimpleClassesLab/04.DistanceBetweenTwoPoints/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.DistanceBetweenTwoPoints
+{
+    class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point firstPoint = ReadPoint();
+            Point secondPoint = ReadPoint();
+
+            double distance = CalcDistance(firstPoint, secondPoint);
+
+            Console.WriteLine($"{distance:f3}");
+        }
+
+        public static Point ReadPoint()
+        {
+            int[] pointParts = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            return new Point
+            {
+                X = pointParts[0],
+                Y = pointParts[1]
+            };
+    }
+
+        public static double CalcDistance(Point firstPoint, Point secondPoint)
+        {
+            var hypotenuse = Math.Sqrt(Math.Pow(firstPoint.X - secondPoint.X, 2) + Math.Pow(firstPoint.Y - secondPoint.Y, 2));
+            return hypotenuse;
+        }
+    }
+}
