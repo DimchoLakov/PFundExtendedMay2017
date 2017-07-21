6163779c12105f8ec91498516564b32cb2df2990
whywherewhat <mdsektor@gmail.com>
2017-07-21 04:28:21 +0100

    Addead 03.BigFactorial

---
+++ 11.ObjectsAndSimpleClassesLab/03.BigFactorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.BigFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger bigFactorial = 1;
+
+            for (int i = n; i > 0; i--)
+            {
+                bigFactorial *= i;
+            }
+            Console.WriteLine(bigFactorial);
+
+        }
+    }
+}
