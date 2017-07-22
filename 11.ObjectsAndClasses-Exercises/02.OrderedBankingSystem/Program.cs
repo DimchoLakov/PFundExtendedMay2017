1f7a351d7b805da3256634c17f9438ab547922dc
whywherewhat <mdsektor@gmail.com>
2017-07-23 00:35:14 +0100

    Added 02.OrderedBankingSystem

---
+++ 11.ObjectsAndClasses-Exercises/02.OrderedBankingSystem/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.OrderedBankingSystem
+{
+    class BankAccount
+    {
+        public string Name { get; set; }
+        public string Bank { get; set; }
+        public decimal Balance { get; set; }
+
+        public static BankAccount Parse(string personName, string bankName, decimal money)
+        {
+            BankAccount bankAcc = new BankAccount
+            {
+                Name = personName,
+                Bank = bankName,
+                Balance = money
+            };
+            return bankAcc;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BankAccount> listBankAccounts = new List<BankAccount>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] tokens = input.Split(new string[] {" | "}, StringSplitOptions.RemoveEmptyEntries);
+
+                string bank = tokens[0];
+                string name = tokens[1];
+                decimal balance = decimal.Parse(tokens[2]);
+                BankAccount newBankAccount = BankAccount.Parse(name, bank, balance);
+
+                listBankAccounts.Add(newBankAccount);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (BankAccount bankAcc in listBankAccounts.OrderByDescending(bal => bal.Balance).ThenBy(bankName => bankName.Bank.Length))
+            {
+                Console.WriteLine($"{bankAcc.Name} -> {bankAcc.Balance} ({bankAcc.Bank})");
+            }
+
+        }
+    }
+}
