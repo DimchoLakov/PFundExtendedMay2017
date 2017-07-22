6d892acd4c4af197744f85b947b212830fd1991e
whywherewhat <mdsektor@gmail.com>
2017-07-22 23:27:11 +0100

    Added 11.ObjectAndClasses-Exercises

---
+++ 11.ObjectsAndClasses-Exercises/01.Exercises/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Exercises
+{
+    class Exercise
+    {
+        public string Topic { get; set; }
+        public string CourseName { get; set; }
+        public string JudgeContestLink { get; set; }
+
+        public List<string> Problems { get; set; }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<Exercise> exercises = new List<Exercise>();
+
+            while (input != "go go go")
+            {
+                Exercise newExercise = new Exercise();
+
+                string[] tokens = input.Split(new string[] { " -> " }, StringSplitOptions.RemoveEmptyEntries);
+                newExercise.Topic = tokens[0];
+                newExercise.CourseName = tokens[1];
+                newExercise.JudgeContestLink = tokens[2];
+
+                string[] problems = tokens[3].Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                newExercise.Problems = new List<string>();
+
+                for (int i = 0; i < problems.Length; i++)
+                {
+                    newExercise.Problems.Add(problems[i]);
+                }
+                exercises.Add(newExercise);
+
+                input = Console.ReadLine();
+            }
+
+
+            foreach (Exercise exercise in exercises)
+            {
+                Console.WriteLine($"Exercises: {exercise.Topic}");
+                Console.WriteLine($"Problems for exercises and homework for the \"{exercise.CourseName}\" course @ SoftUni.");
+                Console.WriteLine($"Check your solutions here: {exercise.JudgeContestLink}");
+                int counter = 1;
+                foreach (string problem in exercise.Problems)
+                {
+                    Console.WriteLine($"{counter}. {problem}");
+                    counter++;
+                }
+            }
+        }
+    }
+}
