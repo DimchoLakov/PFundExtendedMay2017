f7b905c5682e74de1cd051d28f5c581912ca9995
whywherewhat <mdsektor@gmail.com>
2017-07-30 23:13:34 +0100

    Added 02.CountSubstringOccurences

---
+++ 13.StringAndTextProcessing-Lab/02.CountSubstringOccurences/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02.CountSubstringOccurences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenText = Console.ReadLine().ToLower();
+            string wantedString = Console.ReadLine().ToLower();
+
+            int index = givenText.IndexOf(wantedString);
+            int occurences = 0;
+
+            while (index != -1)
+            {
+                index = givenText.IndexOf(wantedString, index + 1);
+                occurences++;
+            }
+            Console.WriteLine(occurences);
+        }
+    }
+}
