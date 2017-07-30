fdeeb805b9237fbd3427d6cc002ae3d9d9f6854f
whywherewhat <mdsektor@gmail.com>
2017-07-30 22:38:25 +0100

    Added 13.StringsAndTextProcessing

---
+++ 13.StringAndTextProcessing-Lab/01.ReverseString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            char[] arrChar = input.ToCharArray().Reverse().ToArray();
+            Console.WriteLine(string.Join("", arrChar));
+        }
+    }
+}
