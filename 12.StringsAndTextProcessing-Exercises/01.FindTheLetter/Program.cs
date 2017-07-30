a4ff41b4c2b1e24ddb425dd3ebf5053fa5a6c681
whywherewhat <mdsektor@gmail.com>
2017-07-31 00:45:35 +0100

    Added 02.Placeholders

---
+++ 12.StringsAndTextProcessing-Exercises/01.FindTheLetter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.FindTheLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+            string[] letterAndOccurences = Console.ReadLine().Split(' ');
+            char wantedLetter = char.Parse(letterAndOccurences[0]);
+            int wantedLetterOccurences = int.Parse(letterAndOccurences[1]);
+
+            char[] arr = inputText.ToCharArray();
+            int count = 0;
+            int index = -1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == wantedLetter)
+                {
+                    count++;
+                    if (count == wantedLetterOccurences)
+                    {
+                        index = i;
+                    }
+                }
+            }
+            if (index != -1)
+            {
+                Console.WriteLine(index);
+            }
+            else
+            {
+                Console.WriteLine($"Find the letter yourself!");
+            }
+            
+        }
+    }
+}
---
+++ 12.StringsAndTextProcessing-Exercises/02.Placeholders/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Placeholders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            while (text != "end")
+            {
+                string[] tokens = text.Split(new string[] { " -> " }, StringSplitOptions.RemoveEmptyEntries);
+
+                string[] elements = tokens[1].Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string sentence = tokens[0];
+
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    if (sentence.Contains($"{{{i}}}"))
+                    {
+                        sentence = sentence.Replace($"{{{i}}}", elements[i]);
+                    }
+                }
+                Console.WriteLine(sentence);
+
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
