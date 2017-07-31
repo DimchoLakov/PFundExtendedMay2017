7ff59f67a7146094f80784205fadc6d9e1af61ca
whywherewhat <mdsektor@gmail.com>
2017-07-31 23:24:44 +0100

    Added 06.SentenceSplit

---
+++ 13.StringsAndTextProcessing-Exercises/06.SentenceSplit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _06.SentenceSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            inputText = inputText.Replace(delimiter, ", ");
+            Console.WriteLine($"[{inputText}]");
+        }
+    }
+}
