fe26876633f935e24f2d892fce6a170aa7e04f74
whywherewhat <mdsektor@gmail.com>
2017-07-31 22:03:19 +0100

    Added 04.JSONParse

---
+++ 13.StringsAndTextProcessing-Exercises/04.JSONParse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.JSONParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            input = input.Substring(2, input.Length - 2 - 2);
+
+            string[] tokens = input.Split(new String[] {"},{"}, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string token in tokens)
+            {
+                string[] props = token.Split(new string[] {",age:", ",grades:"}, StringSplitOptions.RemoveEmptyEntries);
+
+                string name = props[0].Substring(6, props[0].Length - 6 - 1);
+                int age = int.Parse(props[1]);
+                string grades = props[2].Substring(1, props[2].Length - 1 - 1);
+                if (grades == string.Empty)
+                {
+                    grades = "None";
+                }
+                Console.WriteLine($"{name} : {age} -> {grades}");
+            }
+        }
+    }
+}
