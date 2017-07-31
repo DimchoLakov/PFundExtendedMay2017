21b3feaf13b6b80217e63ddfe11b4e6d3099e822
whywherewhat <mdsektor@gmail.com>
2017-07-31 23:46:49 +0100

    Added 07.CapitalizeWords

---
+++ 13.StringsAndTextProcessing-Exercises/07.CapitalizeWords/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace _07.CapitalizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] delimiters = new char[] { ' ', '~', '+', '_', '!', '"', '#', ':', ';', '(', ')', '{', '}', '[', ']',
+                '%', '$', '&', '\\', '/', '=', '|', '*','^', '?', '-', ',','<', '>', '.'};
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] words = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                words = Capitalize(words);
+
+                Console.WriteLine(string.Join(", ", words));
+
+                input = Console.ReadLine();
+            }
+
+        }
+
+        public static string[] Capitalize(string[] words)
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                string firstLetter = words[i].Substring(0, 1).ToUpper();
+                string allOtherLetters = string.Empty;
+                if (words[i].Length > 1)
+                {
+                    allOtherLetters = words[i].Substring(1, words[i].Length - 1).ToLower();
+                }
+                string newWord = firstLetter + allOtherLetters;
+                words[i] = newWord;
+            }
+            return words;
+        }
+    }
+}
