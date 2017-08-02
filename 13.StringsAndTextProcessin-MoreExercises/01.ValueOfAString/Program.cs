8b58df81305e01ad1573c1643601e6667e5836a0
whywherewhat <mdsektor@gmail.com>
2017-08-02 02:18:56 +0100

    Added 01.ValueOfAString Added 02.DiamondProblem

---
+++ 13.StringsAndTextProcessin-MoreExercises/01.ValueOfAString/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ValueOfAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string command = Console.ReadLine();
+            double sum = 0d;
+
+            if (command == "UPPERCASE")
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (char.IsUpper(input[i]))
+                    {
+                        sum += input[i];
+                    }
+                }
+            }
+            else if (command == "LOWERCASE")
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (char.IsLower(input[i]))
+                    {
+                        sum += input[i];
+                    }
+                }
+            }
+            Console.WriteLine($"The total sum is: {sum}");
+        }
+    }
+}
---
+++ 13.StringsAndTextProcessin-MoreExercises/02.DiamondProblem/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.DiamondProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool hasFoundDiamond = false;
+            int startIndex = -1;
+            int endIndex = -1;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '<')
+                {
+                    startIndex = i;
+                }
+                if (input[i] == '>')
+                {
+                    endIndex = i;
+                }
+                if (startIndex < endIndex && startIndex != -1)
+                {
+                    int carats = CalculateCarats(input, startIndex, endIndex);
+                    PrintDiamonds(carats);
+                    startIndex = -1;
+                    endIndex = -1;
+                    hasFoundDiamond = true;
+                }
+            }
+            if (!hasFoundDiamond)
+            {
+                Console.WriteLine($"Better luck next time");
+            }
+        }
+
+        private static void PrintDiamonds(int carats)
+        {
+            if (carats > 0)
+            {
+                Console.WriteLine($"Found {carats} carat diamond");
+            }
+        }
+
+        public static int CalculateCarats(string input, int startIndex, int endIndex)
+        {
+            int sum = 0;
+            for (int i = startIndex + 1; i < endIndex; i++)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    sum += int.Parse(input[i].ToString());
+                }
+            }
+            return sum;
+        }
+    }
+}
