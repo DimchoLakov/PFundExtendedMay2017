153b9db7b7a709d371caff5dc08681371cb2f090
whywherewhat <mdsektor@gmail.com>
2017-08-03 02:01:21 +0100

    Added 06.Stateless

---
+++ 13.StringsAndTextProcessin-MoreExercises/06.Stateless/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06.Stateless
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string state = Console.ReadLine();
+            string fiction = Console.ReadLine();
+
+            while (state != "collapse")
+            {
+                while (fiction.Length > 0)
+                {
+                    state = state.Replace(fiction, string.Empty);
+                    fiction = fiction.Remove(0, 1);
+                    if (fiction.Length == 0)
+                    {
+                        break;
+                    }
+                    fiction = fiction.Remove(fiction.Length - 1, 1);
+                }
+
+                if (state.Length > 0)
+                {
+                    Console.WriteLine(state);
+                }
+                else
+                {
+                    Console.WriteLine($"(void)");
+                }
+                state = Console.ReadLine();
+                if (state == "collapse")
+                {
+                    break;
+                }
+
+                fiction = Console.ReadLine();
+            }
+        }
+    }
+}
