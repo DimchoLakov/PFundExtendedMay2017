7d8a7fde2553dec34c212a52cb47db1d57124a68
whywherewhat <mdsektor@gmail.com>
2017-08-06 17:38:29 +0100

    Added 08.Nilapdromes

---
+++ 13.StringsAndTextProcessin-MoreExercises/08.Nilapdromes/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.Nilapdromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            while (input != "end")
+            {
+                string nilapdrome = GetNilapdrome(input);
+                if (!string.IsNullOrEmpty(nilapdrome))
+                {
+                    Console.WriteLine(nilapdrome);
+                }
+
+                input = Console.ReadLine();
+            }
+            
+        }
+
+        public static string GetNilapdrome(string input)
+        {
+            int middleIndex = input.Length / 2;
+            string border = string.Empty;
+            int leftIndex = 0;
+
+            for (int i = middleIndex + 1; i < input.Length; i++)
+            {
+                if (input[leftIndex] == input[i])
+                {
+                    border += input[i];
+                    leftIndex++;
+                }
+                else
+                {
+                    border = string.Empty;
+                    leftIndex = 0;
+                    if (input[i] == input[leftIndex])
+                    {
+                        border += input[i];
+                        leftIndex++;
+                    }
+                }
+            }
+            
+            string middleContent = string.Empty;
+            if (border != string.Empty)
+            {
+                int leftIndexMiddle = input.IndexOf(border);
+                int rightIndexMiddle = input.LastIndexOf(border);
+
+                middleContent = input.Substring(leftIndexMiddle + border.Length,
+                    rightIndexMiddle - leftIndexMiddle - border.Length);
+                if (middleContent != string.Empty)
+                {
+                    return middleContent + border + middleContent;
+                }
+            }
+
+            return null;
+        }
+    }
+}
