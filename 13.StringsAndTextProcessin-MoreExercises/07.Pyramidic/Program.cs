9374df6d9347ac86534ad018f2aa396c0f358422
whywherewhat <mdsektor@gmail.com>
2017-08-06 16:45:02 +0100

    Added 07.Pyramidic

---
+++ 13.StringsAndTextProcessin-MoreExercises/07.Pyramidic/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.Pyramidic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> lines = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                lines.Add(input);
+            }
+
+            List<string> pyramids = new List<string>();
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string line = lines[i];
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    char character = line[j];
+                    string pyramid = FindPiramids(character, lines, i);
+                    pyramids.Add(pyramid);
+                }
+            }
+
+            string largestPyramid = pyramids.OrderByDescending(p => p.Length).First();
+
+            Console.WriteLine(largestPyramid);
+        }
+
+        private static string FindPiramids(char character, List<string> lines, int lineNum)
+        {
+            int count = 3;
+
+            string pyramid = "" + character + Environment.NewLine;
+
+            for (int i = lineNum + 1; i < lines.Count; i++)
+            {
+                string toFind = new string(character, count);
+
+                if (lines[i].Contains(toFind))
+                {
+                    pyramid += toFind + Environment.NewLine;
+                    count += 2;
+                }
+                else
+                {
+                    return pyramid;
+                }
+            }
+
+            return pyramid;
+        }
+    }
+}
