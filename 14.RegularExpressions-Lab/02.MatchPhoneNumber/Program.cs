aed456f27b49d86c92a45f4c72eb04a8e3ccb87d
whywherewhat <mdsektor@gmail.com>
2017-08-06 18:40:05 +0100

    Added 02.MatchPhoneNumber

---
+++ 14.RegularExpressions-Lab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359([ |-])2\1[0-9]{3}\1[0-9]{4}\b";
+
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+            List<string> phoneNumbers = new List<string>();
+
+            string[] result = matches.Cast<Match>().Select(p => p.Value.Trim()).ToArray();
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
