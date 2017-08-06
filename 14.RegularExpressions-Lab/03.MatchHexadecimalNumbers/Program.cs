09007d7910e8801098661a30a305616b1f3589f2
whywherewhat <mdsektor@gmail.com>
2017-08-06 19:18:31 +0100

    Added 03.HexadecimalNumbers

---
+++ 14.RegularExpressions-Lab/03.MatchHexadecimalNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03.MatchHexadecimalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?:0x)?[0-9A-F]+\b";
+
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            string[] hexNumbers = matches.Cast<Match>().Select(n => n.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(" ", hexNumbers));
+        }
+    }
+}
