ff25518f32f203979fcd8ba58fc45095967f9c1d
whywherewhat <mdsektor@gmail.com>
2017-08-09 00:44:47 +0100

    Variables renamed

---
+++ 14.RegEx-Exercises/03.BoomingCannon/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03.BoomingCannon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] distanceAndCount = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int skipCount = distanceAndCount[0];
+            int takeCount = distanceAndCount[1];
+
+            string pattern = @"(?<=\\<<<)(?<target>[^\\<]+)";
+
+            List<string> result = new List<string>();
+
+            string input = Console.ReadLine();
+
+            Regex cannons = new Regex(pattern);
+
+            MatchCollection matches = cannons.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                string target = string.Empty;
+                if(takeCount >= 0 && skipCount + takeCount < match.Value.Length)
+                {
+                    target = match.Value.Substring(skipCount, takeCount);
+                }
+                else if (takeCount >= 0 && skipCount + takeCount >= match.Value.Length)
+                {
+                    if (skipCount <= match.Value.Length)
+                    {
+                        target = match.Value.Substring(skipCount);
+                    }
+                }
+                if (target != string.Empty)
+                {
+                    result.Add(target);
+                }
+            }
+            Console.WriteLine(string.Join(@"/\", result));
+        }
+    }
+}
