4498be9ebd8ea54022f920ac590a03ca91a5c6ac
whywherewhat <mdsektor@gmail.com>
2017-08-08 21:54:43 +0100

    Added 01.Cards

---
+++ 14.RegEx-Exercises/01.Cards/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01.Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(?<=[SHDC]|^)([2-9JQKA]|10)[SHDC]";
+
+            Regex cards = new Regex(pattern);
+
+            MatchCollection matches = cards.Matches(input);
+
+            string[] result = matches.Cast<Match>().Select(card => card.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
