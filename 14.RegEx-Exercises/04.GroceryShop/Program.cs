0698e350032f632d198540b61e6cc3891daa370e
whywherewhat <mdsektor@gmail.com>
2017-08-09 01:01:49 +0100

    Added 04.GroceryShop

---
+++ 14.RegEx-Exercises/04.GroceryShop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _04.GroceryShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, double> products = new Dictionary<string, double>();
+
+            string pattern = @"\b[A-Z][a-z]+:\d+\.\d{2}$";
+
+            while (input != "bill")
+            {
+                Regex product = new Regex(pattern);
+
+                MatchCollection matches = product.Matches(input);
+
+                foreach (Match match in matches)
+                {
+                    string[] nameAndPrice = match.Value.Split(':');
+                    string productName = nameAndPrice[0];
+                    double productPrice = double.Parse(nameAndPrice[1]);
+                    if (! products.ContainsKey(productName))
+                    {
+                        products.Add(productName, productPrice);
+                    }
+                    else
+                    {
+                        products[productName] = productPrice;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (KeyValuePair<string, double> product in products.OrderByDescending(p => p.Value))
+            {
+                Console.WriteLine($"{product.Key} costs {product.Value:f2}");
+            }
+        }
+    }
+}
