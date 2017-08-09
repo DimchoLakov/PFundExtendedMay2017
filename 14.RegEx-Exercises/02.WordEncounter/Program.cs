1ccef33db883e9ef9cc9b38583dc289a287d2d25
whywherewhat <mdsektor@gmail.com>
2017-08-09 02:14:32 +0100

    Added 05.SoftUniMessages

---
+++ 14.RegEx-Exercises/02.WordEncounter/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.WordEncounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filter = Console.ReadLine();
+            char filterChar = filter[0];
+            int filterCharCount = int.Parse(filter[1].ToString());
+
+            List<string> words = new List<string>();
+
+            string sentencePattern = @"^[A-Z].*?[\.!?]$";
+            string wordsPattern = @"\w+";
+
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                Regex sentence = new Regex(sentencePattern);
+
+                if (sentence.IsMatch(input))
+                {
+                    MatchCollection wordsInSentence = Regex.Matches(input, wordsPattern);
+                    string[] wordsToCheck = wordsInSentence
+                        .Cast<Match>()
+                        .Select(w => w.Value.Trim())
+                        .Where(w => w.Contains(filterChar))
+                        .ToArray();
+                    foreach (string word in wordsToCheck)
+                    {
+                        int count = word.Length - word.Replace($"{filterChar}", "").Length;
+                        if (count >= filterCharCount)
+                        {
+                            words.Add(word);
+                        }
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(", ", words));
+        }
+    }
+}
---
+++ 14.RegEx-Exercises/05.SoftUniMessages/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _05.SoftUniMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            string pattern = @"^(?<first>\d+)(?<content>[A-Za-z]+)(?<second>\d+)$";
+
+            int count = 1;
+
+            while (true)
+            {
+                if (count % 2 != 0)
+                {
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    int searchedLength = int.Parse(Console.ReadLine());
+
+                    Regex regex = new Regex(pattern);
+                    Match match = regex.Match(input);
+
+                    var content = match.Groups["content"];
+
+                    if (content.Length == searchedLength)
+                    {
+                        string firstGroup = match.Groups["first"].ToString();
+                        string secondGroup = match.Groups["second"].ToString();
+
+                        string result = FormStringFromIndexes(content.ToString(), firstGroup, secondGroup);
+                        Console.WriteLine($"{match.Groups["content"]} = {result}");
+                    }
+                }
+                if (input == "Decrypt!")
+                {
+                    break;
+                }
+                count++;
+            }
+
+        }
+
+        public static string FormStringFromIndexes(string content, string firstGroup, string secondGroup)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(' ', 100);
+            int[] firstIndexes = new int[firstGroup.Length];
+            int[] secondIndexes = new int[secondGroup.Length];
+            for (int i = 0; i < firstGroup.Length; i++)
+            {
+                firstIndexes[i] = int.Parse(firstGroup[i].ToString());
+            }
+            for (int i = 0; i < secondGroup.Length; i++)
+            {
+                secondIndexes[i] = int.Parse(secondGroup[i].ToString());
+            }
+
+            for (int j = 0; j < firstIndexes.Length; j++)
+            {
+
+                if (content.Length > firstIndexes[j])
+                {
+                    stringBuilder.Append(content[firstIndexes[j]]);
+                }
+            }
+            for (int j = 0; j < secondIndexes.Length; j++)
+            {
+                if (content.Length > secondIndexes[j])
+                {
+                    stringBuilder.Append(content[secondIndexes[j]]);
+                }
+            }
+            string result = stringBuilder.ToString().Trim();
+
+            return result;
+        }
+    }
+}
