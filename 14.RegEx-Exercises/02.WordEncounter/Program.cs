b13b5fcd381b16cd3514e3cfd421099611a7e372
whywherewhat <mdsektor@gmail.com>
2017-08-10 00:08:14 +0100

    Added 02.WordEncounter Added 06.FishStatistics

---
+++ 14.RegEx-Exercises/02.WordEncounter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.WordEncounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filter = Console.ReadLine();
+            string filterChar = $"{filter[0]}";
+            int filterCharCount = int.Parse(filter[1].ToString());
+
+            List<string> words = new List<string>();
+
+            string sentencePattern = @"^[A-Z].*[.!?]$";
+            string wordsPattern = @"\w+";
+
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                Regex sentence = new Regex(sentencePattern);
+                if (sentence.IsMatch(input))
+                {
+                    MatchCollection wordsInSentence = Regex.Matches(input, wordsPattern);
+                    
+                    foreach (Match word in wordsInSentence)
+                    {
+                        int count = Regex.Matches($"{word}", $@"{filterChar}").Count;
+                        if (count >= filterCharCount)
+                        {
+                            words.Add(word.ToString());
+                        }
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(", ", words));
+        }
+    }
+}
---
+++ 14.RegEx-Exercises/06.FishStatistics/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06.FishStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Regex fishPattern = new Regex(@">*<\(+['\-x]>");
+
+            string input = Console.ReadLine();
+
+            MatchCollection fishMatches = fishPattern.Matches(input);
+            int fishCount = 1;
+            foreach (Match fishMatch in fishMatches)
+            {
+                int tailLength = 0;
+                int bodyLength = 0;
+                string status = string.Empty;
+                foreach (char ch in fishMatch.Value)
+                {
+                    switch (ch)
+                    {
+                        case '>':
+                            tailLength++;
+                            break;
+                        case '(':
+                            bodyLength++;
+                            break;
+                        case '\'':
+                            status = "Awake";
+                            break;
+                        case 'x':
+                            status = "Dead";
+                            break;
+                        case '-':
+                            status = "Asleep";
+                            break;
+                    }
+                }
+                tailLength--;
+                string tailType = GetTailType(tailLength);
+                string bodyType = GetBodyType(bodyLength);
+                
+                Console.WriteLine($"Fish {fishCount}: {fishMatch.Value}");
+                Console.WriteLine(tailLength > 0
+                    ? $"  Tail type: {tailType} ({tailLength * 2} cm)"
+                    : $"  Tail type: {tailType}");
+                Console.WriteLine($"  Body type: {bodyType} ({bodyLength * 2} cm)");
+                Console.WriteLine($"  Status: {status}");
+                fishCount++;
+            }
+            if (fishCount == 1)
+            {
+                Console.WriteLine($"No fish found.");
+            }
+        }
+
+        static string GetBodyType(int bodyLength)
+        {
+            if (bodyLength > 10)
+            {
+                return "Long";
+            }
+            else if (bodyLength > 5)
+            {
+                return "Medium";
+            }
+            else
+            {
+                return "Short";
+            }
+        }
+
+        static string GetTailType(int tailLength)
+        {
+            if (tailLength > 5)
+            {
+                return "Long";
+            }
+            else if (tailLength > 1)
+            {
+                return "Medium";
+            }
+            else if (tailLength == 1)
+            {
+                return "Short";
+            }
+            else
+            {
+                return "None";
+            }
+        }
+    }
+}
