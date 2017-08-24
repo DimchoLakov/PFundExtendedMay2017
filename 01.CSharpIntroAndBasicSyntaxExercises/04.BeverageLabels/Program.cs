72cf7817fbb335ffd40f19cbc3cfcc3cab9f65a1
whywherewhat <mdsektor@gmail.com>
2017-08-24 02:41:01 +0100

    Added 04.BeverageLabels

---
+++ 01.CSharpIntroAndBasicSyntaxExercises/04.BeverageLabels/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04.BeverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string productName = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energy = int.Parse(Console.ReadLine());
+            int sugar = int.Parse(Console.ReadLine());
+
+            double energyForOneMl = energy / 100.0;
+            double sugerForOneMl = sugar / 100.0;
+            double energyForGivenVolume = energyForOneMl * volume;
+            double sugarForGivenVolume = sugerForOneMl * volume;
+
+            Console.WriteLine($"{volume}ml {productName}:");
+            Console.WriteLine($"{energyForGivenVolume}kcal, {sugarForGivenVolume}g sugars");
+        }
+    }
+}
