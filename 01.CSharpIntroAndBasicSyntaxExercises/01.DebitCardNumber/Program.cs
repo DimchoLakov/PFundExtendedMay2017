14e301b179c87fd279a505d7ec73bf99d38122cb
whywherewhat <mdsektor@gmail.com>
2017-08-24 02:30:09 +0100

    Added 01.CSharpIntro-Exercises

---
+++ 01.CSharpIntroAndBasicSyntaxExercises/01.DebitCardNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.DebitCardNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstPairDigits = int.Parse(Console.ReadLine());
+            int secondPairDigits = int.Parse(Console.ReadLine());
+            int thirdPairDigits = int.Parse(Console.ReadLine());
+            int fourthPairDigits = int.Parse(Console.ReadLine());
+
+            string result = $"{firstPairDigits:D4} {secondPairDigits:D4} {thirdPairDigits:D4} {fourthPairDigits:D4}";
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 01.CSharpIntroAndBasicSyntaxExercises/03.MilesToKilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _03.MilesToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miles = double.Parse(Console.ReadLine());
+            double milesToKilometers = miles * 1.60934;
+            Console.WriteLine($"{milesToKilometers:f2}");
+        }
+    }
+}
