fef001b82ae8b83592fec82abbef70ee0919a435
whywherewhat <mdsektor@gmail.com>
2017-08-24 03:10:48 +0100

    Added First 3 Problems

---
+++ 02.CSharpConditionalStatementsAndLoops-Lab/03.BackIn30Minutes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03.BackIn30Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int totalMinutes = hours * 60 + minutes + 30;
+            int resultHours = totalMinutes / 60;
+            int resultMinutes = totalMinutes % 60;
+
+            if (resultMinutes > 59)
+            {
+                resultHours--;
+            }
+            if (resultHours > 23)
+            {
+                resultHours -= 24;
+            }
+            Console.WriteLine($"{resultHours}:{resultMinutes:00}");
+        }
+    }
+}
