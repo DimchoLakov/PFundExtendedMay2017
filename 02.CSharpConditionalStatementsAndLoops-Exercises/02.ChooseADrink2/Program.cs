e53b4e86461187ec18c5599153c4b3ef595f979e
whywherewhat <mdsektor@gmail.com>
2017-09-09 22:42:45 +0100

    Added 02.ChooseADrink2

---
+++ 02.CSharpConditionalStatementsAndLoops-Exercises/02.ChooseADrink2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02.ChooseADrink2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string profession = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            string drink = string.Empty;
+            double price = 0d;
+
+            if (profession == "Athlete")
+            {
+                drink = "Water";
+                price = 0.7;
+            }
+            else if (profession == "Businessman" || profession == "Businesswoman")
+            {
+                drink = "Coffee";
+                price = 1.00;
+            }
+            else if (profession == "SoftUni Student")
+            {
+                drink = "Beer";
+                price = 1.7;
+            }
+            else
+            {
+                drink = "Tea";
+                price = 1.2;
+            }
+            double total = price * quantity;
+            Console.WriteLine($"The {profession} has to pay {total:f2}.");
+        }
+    }
+}
